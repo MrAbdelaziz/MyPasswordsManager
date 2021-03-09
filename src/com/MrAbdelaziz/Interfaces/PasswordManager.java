5b5bec43139f2073bf403ec79720d1d652f40c1b
Abdelaziz ELOUAHAB <elouahab.abdelaziz@gmail.com>
2021-03-09 14:45:07 +0100

    Initial commit

---
+++ src/com/MrAbdelaziz/Interfaces/PasswordManager.java
@@ -0,0 +1,4 @@
+package com.MrAbdelaziz.Interfaces;
+
+public interface PasswordManager {
+}
---
+++ src/com/MrAbdelaziz/Main.java
@@ -0,0 +1,8 @@
+package com.MrAbdelaziz;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
